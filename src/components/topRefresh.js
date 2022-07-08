8f5aeddb7fa34c53c05b64a884b7c4dfa3c3fa0b
TheCubiq <jlajsek@gmail.com>
2022-07-09 00:50:18 +0200

    created new component and imported to main app

---
+++ src/components/topRefresh.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+
+const TopRefresh = (props) =>  {
+    return (
+      <View>
+        <Text>topRefresh</Text>
+      </View>
+    )
+};
+
+export default TopRefresh;
+
+// const styles = StyleSheet.create({
+    
+// })
