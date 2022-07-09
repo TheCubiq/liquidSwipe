0987cd146d787aa55cd7b450cf6241322571aa47
TheCubiq <jlajsek@gmail.com>
2022-07-10 00:41:25 +0200

    kind of refresh xd

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Platform, StyleSheet, Text, View } from "react-native";
 import TopRefresh from "./src/components/topRefresh";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <TopRefresh>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </TopRefresh>
-    </View>
+    <>
+      <StatusBar
+        // translucent={true}
+        // style="auto"
+        hidden
+      />
+      <View style={styles.container}>
+        <TopRefresh>
+          {[...Array(100)].map((_, i) => (
+            <Text
+              style={{
+                padding: 10,
+                margin: 10,
+                fontSize: 50,
+                color: "#ffffff",
+                borderRadius: 10,
+                backgroundColor:
+                  // `rgb(
+                  //   ${Math.floor(Math.random() * 256)},
+                  //   ${Math.floor(Math.random() * 256)},
+                  //   ${Math.floor(Math.random() * 256)}
+                  //   )`,
+
+                  // `${i % 2 === 0 ? "red" : "blue"}`,
+                  
+                  // rainbow pattern
+                  `hsl(
+                    ${Math.floor(i * 10)},
+                    75%, 50%)`,
+
+                textAlign: "center",
+                textAlignVertical: "center",
+              }}
+              key={i}
+            >
+              {i + 1}
+            </Text>
+          ))}
+        </TopRefresh>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
--- src/components/topRefresh.js
+++ src/components/topRefresh.js
@@ -1,17 +1,80 @@
-import React, { Component } from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import React, { Component } from "react";
+import { Text, View, StyleSheet } from "react-native";
+import Animated, {
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  useSharedValue,
+  withDecay,
+  withSpring,
+} from "react-native-reanimated";
+import {
+  GestureHandlerRootView,
+  PanGestureHandler,
+} from "react-native-gesture-handler";
 
-const TopRefresh = (props) =>  {
-    return (
-      <View>
-        <Text>topRefresh</Text>
-        {props.children}
-      </View>
-    )
+const SPRING_CONFIG = {
+  damping: 80,
+  overshootClamping: true,
+  restDisplacementThreshold: 0.1,
+  restSpeedThreshold: 0.1,
+  stiffness: 500,
+};
+
+const TopRefresh = (props) => {
+  // const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+
+  const rStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        // { translateX: withSpring(translateX.value, SPRING_CONFIG) },
+      { translateY: withSpring(translateY.value, SPRING_CONFIG) },
+      ],
+    };
+  });
+
+  const panGestureEvent = useAnimatedGestureHandler({
+    onStart: (_, ctx) => {
+      ctx.y = translateY.value;
+      // ctx.x = translateX.value;
+    },
+    onActive: (event, ctx) => {
+      const currentPosX = ctx.x + event.translationX;
+      const currentPosY = ctx.y + event.translationY;
+      translateY.value = currentPosY;
+      // translateX.value = currentPosX;
+    },
+    onEnd: (event) => {
+      if (translateY.value > 0) {
+        translateY.value = 0;
+      }
+    },
+  });
+
+  return (
+    <GestureHandlerRootView style={styles.container}>
+      <Animated.View>
+        <Text
+          style={{
+            position: "absolute",
+            top: 0,
+          }}
+        >
+          topRefresh
+        </Text>
+        <PanGestureHandler onGestureEvent={panGestureEvent}>
+          <Animated.View style={[rStyle]}>{props.children}</Animated.View>
+        </PanGestureHandler>
+      </Animated.View>
+    </GestureHandlerRootView>
+  );
 };
 
 export default TopRefresh;
 
-// const styles = StyleSheet.create({
-    
-// })
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
